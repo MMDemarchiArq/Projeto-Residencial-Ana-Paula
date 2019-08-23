07292c63756b73f8eb84a92b46f2f1e5710d224b
MMDemarchiArq <50173122+MMDemarchiArq@users.noreply.github.com>
2019-08-23 09:01:06 -0300

    Projeto

---
+++ data.js
@@ -0,0 +1,697 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-fachada",
+      "name": "Fachada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5691274295837765,
+        "pitch": 0.30225796806744754,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.3034265247814112,
+          "pitch": 0.41636391210819923,
+          "rotation": 0,
+          "target": "5-entrada"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-banheiro",
+      "name": "Banheiro",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6571691150761225,
+        "pitch": 0.25368406025902246,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6529502563074665,
+          "pitch": 0.41786159208249885,
+          "rotation": 7.853981633974483,
+          "target": "3-corredor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.771024853173511,
+          "pitch": -0.1994054177846074,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Revestimento:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Pastilha Azul</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "2-banheiro-suite",
+      "name": "Banheiro Suite",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.562230684872242,
+        "pitch": 0.3961446697742268,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.5762322776454383,
+          "pitch": 0.5487929082392142,
+          "rotation": 1.5707963267948966,
+          "target": "13-sute-master"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.602024249428446,
+          "pitch": -0.09921956897131423,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Revestimento:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Pastilha Azul</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "3-corredor",
+      "name": "Corredor",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5464118843881831,
+        "pitch": 0.27166048183030966,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4290415481580556,
+          "pitch": 0.8332209085150808,
+          "rotation": 3.141592653589793,
+          "target": "11-sala-de-estar"
+        },
+        {
+          "yaw": -2.0994185470409903,
+          "pitch": 0.2810114475225305,
+          "rotation": 4.71238898038469,
+          "target": "1-banheiro"
+        },
+        {
+          "yaw": -0.968155336051117,
+          "pitch": 0.2887110170506624,
+          "rotation": 1.5707963267948966,
+          "target": "10-quarto-de-hspedes"
+        },
+        {
+          "yaw": -1.2135426787842487,
+          "pitch": 0.19165183596492952,
+          "rotation": 1.5707963267948966,
+          "target": "9-quarto"
+        },
+        {
+          "yaw": -1.4271348559719907,
+          "pitch": 0.09277837184804127,
+          "rotation": 0,
+          "target": "13-sute-master"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-cozinha",
+      "name": "Cozinha",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.401053986716306,
+        "pitch": 0.26573713702106616,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.229697209279177,
+          "pitch": 0.21051060777339003,
+          "rotation": 1.5707963267948966,
+          "target": "6-espao-gourmet"
+        },
+        {
+          "yaw": -2.378878581886779,
+          "pitch": 0.21745594647858368,
+          "rotation": 4.71238898038469,
+          "target": "8-lavanderia"
+        },
+        {
+          "yaw": -1.7321854431497687,
+          "pitch": 0.8023459994140865,
+          "rotation": 3.141592653589793,
+          "target": "12-sala-de-jantar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1742987532201496,
+          "pitch": 0.03641851961535991,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Revestimento:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Azulejo Português Colorido</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "5-entrada",
+      "name": "Entrada",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5392361761753364,
+        "pitch": 0.14851019226100703,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.6088574091989223,
+          "pitch": 0.7172304670394531,
+          "rotation": 3.141592653589793,
+          "target": "0-fachada"
+        },
+        {
+          "yaw": -1.267610152639243,
+          "pitch": 0.34470498796465066,
+          "rotation": 0,
+          "target": "6-espao-gourmet"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6423562453988723,
+          "pitch": -0.16853327140348995,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Revestimento:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Petra Acetinado</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "6-espao-gourmet",
+      "name": "Espaço Gourmet",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6001178582283941,
+        "pitch": 0.30757477086983087,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6733505386220919,
+          "pitch": 0.29656687702435214,
+          "rotation": 1.5707963267948966,
+          "target": "5-entrada"
+        },
+        {
+          "yaw": -2.283184262778059,
+          "pitch": 0.3317649903498694,
+          "rotation": 4.71238898038469,
+          "target": "4-cozinha"
+        },
+        {
+          "yaw": -2.667754782317889,
+          "pitch": 0.48690035390387365,
+          "rotation": 4.71238898038469,
+          "target": "12-sala-de-jantar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.0828712329555685,
+          "pitch": 0.013151464051507133,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Revestimento:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Azulejo Português Colorido</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "7-jardim",
+      "name": "Jardim",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5573701004819682,
+        "pitch": 0.3209005744219571,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.5387363690817608,
+          "pitch": 0.8980702179953663,
+          "rotation": 3.141592653589793,
+          "target": "13-sute-master"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "8-lavanderia",
+      "name": "Lavanderia",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.4662571627371257,
+        "pitch": 0.31552172032530024,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.4807664773237494,
+          "pitch": 0.3016364072214266,
+          "rotation": 0,
+          "target": "4-cozinha"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1808107231716676,
+          "pitch": 0.037188816531037006,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Revestimento:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Pastilha Verde</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "9-quarto",
+      "name": "Quarto",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.6775280632869194,
+        "pitch": 0.22486255762192542,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.6719013412097183,
+          "pitch": 0.1695672713369838,
+          "rotation": 4.71238898038469,
+          "target": "3-corredor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.1084450073699799,
+          "pitch": 0.06775742670074791,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Papel de Parede:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Tijolo à Vista</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "10-quarto-de-hspedes",
+      "name": "Quarto de Hóspedes",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5970243896492065,
+        "pitch": 0.20750812528210005,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.9554327280447588,
+          "pitch": 0.7298382002346351,
+          "rotation": 3.141592653589793,
+          "target": "3-corredor"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.5429021939179997,
+          "pitch": -0.27676289128214293,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Papel de Parede:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Cimento Queimado</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "11-sala-de-estar",
+      "name": "Sala de Estar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.4326249195946268,
+        "pitch": 0.34641770130927796,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.2266811962801825,
+          "pitch": 0.27844939847360983,
+          "rotation": 0,
+          "target": "3-corredor"
+        },
+        {
+          "yaw": -1.0399838280051092,
+          "pitch": 0.8677080245627149,
+          "rotation": 3.141592653589793,
+          "target": "12-sala-de-jantar"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -2.1591655553304108,
+          "pitch": -0.1356721122800444,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Papel de Parede:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Tijolo à Vista Branco</font></font>"
+        }
+      ]
+    },
+    {
+      "id": "12-sala-de-jantar",
+      "name": "Sala de Jantar",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.290254671094747,
+        "pitch": 0.3201769484156092,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.1445284072546684,
+          "pitch": 0.25870748165728585,
+          "rotation": 0,
+          "target": "6-espao-gourmet"
+        },
+        {
+          "yaw": -1.9825798740062428,
+          "pitch": 0.30591442258850776,
+          "rotation": 5.497787143782138,
+          "target": "4-cozinha"
+        },
+        {
+          "yaw": -2.1742616758807873,
+          "pitch": 0.6397895067160171,
+          "rotation": 3.9269908169872414,
+          "target": "11-sala-de-estar"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "13-sute-master",
+      "name": "Suíte Master",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -1.5324812163922878,
+        "pitch": 0.13436544650680915,
+        "fov": 1.2691192001910299
+      },
+      "linkHotspots": [
+        {
+          "yaw": -2.5038720065894307,
+          "pitch": 0.13395894041852863,
+          "rotation": 4.71238898038469,
+          "target": "2-banheiro-suite"
+        },
+        {
+          "yaw": -2.350389934172652,
+          "pitch": 0.5241255174358539,
+          "rotation": 10.210176124166829,
+          "target": "3-corredor"
+        },
+        {
+          "yaw": -0.9316125108067581,
+          "pitch": 0.6136148146439613,
+          "rotation": 8.63937979737193,
+          "target": "7-jardim"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": -1.6702388866853504,
+          "pitch": -0.2746904560327046,
+          "title": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Papel de Parede:</font></font>",
+          "text": "<font style=\"vertical-align: inherit;\"><font style=\"vertical-align: inherit;\">Folhas</font></font>"
+        }
+      ]
+    }
+  ],
+  "name": "Project Title",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
